1cd7edc210d842370da747f6e23a5dd91ff2513c
Wesley Reynolds <weslezjnz@gmail.com>
2017-12-23 13:06:08 -0500

    inside

--- games/FreakingAwesomeNinjaDude/fand.js
+++
@@ -1,6 +0,0 @@
-var canvas = document.getElementById("canv");
-var ctx = canvas.getContext("2d");
-window.onload function(){
-	ctx.fillStyle = "black";
-	ctx.fillRect(0,0,canvas.width,canvas.height)
-}
