bf60182284e8cfff272b86eb62e692dd6353f943
Wesley Reynolds <weslezjnz@gmail.com>
2017-12-23 12:24:11 -0500

    gamechange

--- games/FreakingAwesomeNinjaDude/FreakingAwesomeNinjaDude.js
+++ games/FreakingAwesomeNinjaDude/FreakingAwesomeNinjaDude.js
@@ -1,7 +1,25 @@
+var gHeight = window.innerHeight; //Used for ground.
+var player, ground1; //Image for player, and level 1 ground!.
+var x; //X position.
+var vx; //X velocity.
+var y; //Y position.
+var gravity; //Basically Y velocity, or vx.
+
+function preload(){
+  player = loadImage("Sprites/Entities/Player.png");
+  ground1 = loadImage("/Sprites/World/Ground1.png");
+}
+
 function setup() {
-  createCanvas(400,400);
+  preload();
+  x = 200;
+  y = 200;
+  createCanvas(window.innerWidth,window.innerHeight);
 }
 
 function draw() {
-   rectangle(50,50,20,20);
+  noStroke();
+  background(0, 228, 244);
+  image(player, x, y);
+  image(ground1, 0, gHeight+10, ground1.width*200, ground1.height);
 }
